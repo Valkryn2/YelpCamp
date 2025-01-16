9e5669400ecf7fd7cfa8fe396855bd54cdf53e13
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2025-01-16 13:07:34 -0500

    before seed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const express = require("express");
+const app = express();
+const path = require("path");
+const CampGround = require("./models/campground");
+const port = 3000;
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelpCamp");
+
+const db = mongoose.connection;
+// db.on wait for response and once the selected response , 'error' occurs the the 2nd parameter occurs.
+db.on("error", console.error.bind(console, "Connection Error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+// allows the use of EJS in JavaScripts
+app.set("view engine", "ejs");
+// ensure that the views folder is used no matter where the app is called from.
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(port, () => {
+  console.log("you are connected");
+});
+
+app.get("/", (req, resp) => {
+  resp.render("home");
+});
+
+app.get("/makecamp", async (req, resp) => {
+  const camp = new CampGround({ name: "backyard", description: "free" });
+  await camp.save();
+  resp.send(camp);
+});
